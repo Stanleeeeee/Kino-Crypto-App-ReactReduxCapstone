315ee6a71a0d0611ff9f57f6cc9bb21bb4d2b56b
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-07-09 20:55:02 +0300

    Add jest test file

---
+++ src/__tests__/CoinsDetail.test.js
@@ -0,0 +1,24 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-redux';
+import Coins from '../components/Coins';
+import App from '../App';
+import store from '../redux/configureStore';
+import { fetchGetCoins } from '../redux/coins';
+import CoinsDetail from '../components/coins/CoinsDetail';
+
+describe('Testing detail', () => {
+  it('testing click interaction', async () => {
+    await store.dispatch(fetchGetCoins());
+    render(
+      <Provider store={store}>
+        <App>
+          <CoinsDetail />
+        </App>
+      </Provider>,
+    );
+    const TetherDetail = screen.getByText('Tether - USDT');
+    fireEvent.click(TetherDetail);
+    expect(screen.getByText('Cryptocurrency')).toMatchSnapshot();
+  });
+});
